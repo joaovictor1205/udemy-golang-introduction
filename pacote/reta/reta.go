be67ea2d5fe208f98f82f06b899c5cd1af98940d
João Victor da Costa Gonçalves <joaog@kyros.com.br>
2019-10-04 11:48:18 -0300

    pacote add

---
+++ pacote/reta/reta.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"math"
+)
+
+// letra maiscula =  Public  (visivel dentro e fora do pacote)
+// letra minuscula = Private (visivel apenas dentro do pacote)
+
+//Ponto representa um valor no plano cartesiano
+type Ponto struct{
+	x float64
+	y float64
+}
+
+func catetos(a,b Ponto) (cx, cy float64){
+	cx = math.Abs(b.x - a.x)
+	cy = math.Abs(b.y - a.y)
+
+	return
+}
+
+//Calcular distancia linear entre os pontos
+func Distancia(a,b Ponto) float64{
+	cx ,cy := catetos(a,b)
+	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))
+}
