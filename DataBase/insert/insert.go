5e90e3bee71c3bf9745cffdcdb2f2a2f5e3b768c
joaovictor1205 <joaovictor1205>
2019-10-07 23:00:32 +0000

    insert add

---
+++ DataBase/insert/insert.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	db, err := sql.Open("mysql", "root:1234@/cursoGolang")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	stmt, _ := db.Prepare("insert into usuarios(nome) values(?)")
+	stmt.Exec("Maria")
+	stmt.Exec("Joao")
+
+	res, _ := stmt.Exec("Pedro")
+
+	id, _ := res.LastInsertId()
+	fmt.Println(id)
+
+	linhas, _ := res.RowsAffected()
+	fmt.Println(linhas)
+
+}
